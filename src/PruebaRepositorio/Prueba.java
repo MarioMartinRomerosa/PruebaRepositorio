aa9b604d05b9f2d5da02752b33782fcc4cb48475
MarioMartinRomerosa <mario.mromerosa@alumnos.upm.es>
2025-10-05 22:13:56 +0200

    Initial commit

---
+++ src/PruebaRepositorio/Prueba.java
@@ -0,0 +1,14 @@
+package PruebaRepositorio;
+
+public class Prueba {
+	
+	
+	static String a= "primera";
+	static String b= "entrega";
+
+	public static void main(String[] args) {
+		
+System.out.println("Esta es la " + a + " "+ b +" de github");
+	}
+
+}
